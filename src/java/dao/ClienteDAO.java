f0458fb0c0f9cbf877df9c25800e6f8670cf028b
Carlos Marinho <cadumsantos@hotmail.com>
2022-09-21 07:18:59 -0300

    ConexaoFactory implementacao

--- src/java/dao/ClienteDAO.java
+++ src/java/dao/ClienteDAO.java
@@ -1,16 +1,6 @@
 package dao;
 
+public class ClienteDAO {
 
-import java.util.List;
-import model.Cliente;
-
-public interface ClienteDAO {
-
-	void insert(Cliente obj);
-	void update(Cliente obj);
-	void deleteById(Integer id);
-	
-	Cliente findById(Integer id);
-	List<Cliente> findAll();
 
 }
